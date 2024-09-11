aed6490f310c420eaec1c12ea25f7d272ff77664
D3asyn <konkolykovacsd@gmail.com>
2024-09-11 13:09:03 +0200

    add remove from list

--- egy.js
+++ egy.js
@@ -17,11 +17,26 @@ function add() {
 
 var bl1 = [];
 
+function remove(x) {
+	bl1.splice(bl1.indexOf(x), 1);
+	console.log("removed");
+
+	document.querySelector("#lista").innerHTML = bl1
+		.map(
+			(elem, i) =>
+				`<li class="x${i % 2}">${elem}</li><button onclick="remove('${elem}')">Remove</button>`,
+		)
+		.join("");
+}
+
 function lista(x) {
 	bl1.push(x.value);
 	bl1.sort();
 	x.value = "";
 	document.querySelector("#lista").innerHTML = bl1
-		.map((elem, i) => `<li class="x${i % 2}">${elem}</li>`)
+		.map(
+			(elem, i) =>
+				`<li class="x${i % 2}">${elem}</li><button onclick="remove('${elem}')">Remove</button>`,
+		)
 		.join("");
 }
